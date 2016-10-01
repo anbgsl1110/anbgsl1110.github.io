dc14759c211efd3ead533694d94d43092472b98d
aliang <anbgsl@al1110.com>
2016-10-02 00:32:10 +0800

    add static

---
+++ static/dep/ckfinder/ckfinder.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+
+public partial class ckfinder : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //网站后台上传权限验证
+        if (Request.UrlReferrer == null || (Request.UrlReferrer.AbsoluteUri.ToLower().Contains("/cms/") && Session["AdminInfo"] == null))
+        {
+            Response.Write("403");
+            Response.End();
+        }
+    }
+}
